f8102cf61069f1b567784155c1b11bd3bf4930b0
thejunghare <prasadjunghare2@gmail.com>
2026-04-23 14:05:08 +0530

    updated 23 april

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -4,31 +4,88 @@ using namespace std;
 
 struct Node {
   int data;
-  Node *add;
+  Node *address;
 };
 
 // void insert(Node *newnode, int data) {
 //   Node *temp = new Node;
 //   temp->data = data;
-//   temp->add = NULL;
+//   temp->address = NULL;
 // }
 
-int main() {
+void printList(Node *head) {
+  Node *temp = head;
+  while (temp != NULL) {
+    cout << temp->data << " -> ";
+    temp = temp->address;
+  }
+  cout << "NULL" << endl;
+}
 
+void searchValue(Node *head, int value) {
+  if (head == NULL) {
+    cout << "linked list is empty" << endl;
+    return;
+  }
+
+  Node *temp = head;
+  int post = 0;
+  while (temp != NULL) {
+    if (temp->data == value) {
+      cout << "value exist at index " << post << endl;
+      return;
+    }
+    temp = temp->address;
+    post++;
+  }
+
+  cout << "Value doesnot exist" << -1 << endl;
+}
+
+void insertAtHead(Node *&head, int value) {
+  Node *newNode = new Node;
+
+  // if (head == NULL) {
+  // set the number and do not change address
+  newNode->data = value;
+  newNode->address = head;
+  head = newNode;
+
+  // } else {
+  //   // store the number and change
+
+  // }
+}
+
+void insertAtEnd(Node *&head, int value) {
+  Node *newNode = new Node;
+  newNode->data = value;
+  newNode->address = NULL;
+  if (head == NULL) {
+    head = newNode;
+    return;
+  }
+
+  // while
+  Node *temp = head;
+  while (temp->address != NULL) {
+    temp = temp->address; // 30
+  }
+  temp->address = newNode;
+}
+
+int main() {
   Node *head = new Node;
   Node *second = new Node;
 
   head->data = 70;
-  head->add = second;
+  head->address = second;
 
   second->data = 30;
-  second->add = NULL;
+  second->address = NULL;
   // insert(newNode, 70);
-
-  Node *temp = head;
-  while (temp != NULL) {
-    cout << temp->data << " -> ";
-    temp = temp->add;
-  }
-  cout << "NULL" << endl;
+  searchValue(head, 50);
+  insertAtHead(head, 40);
+  insertAtEnd(head, 100);
+  printList(head);
 }
